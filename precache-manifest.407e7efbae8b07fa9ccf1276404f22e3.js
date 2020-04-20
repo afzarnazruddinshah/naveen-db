a8f1a0f424bb19762859ef3fb292030cbec6cd98
livingstone pilavendran <livingstone.it@gmail.com>
2020-04-20 23:58:54 +0530

    Updates

---
+++ precache-manifest.407e7efbae8b07fa9ccf1276404f22e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31ed95687c410dc6acae159cb4063a3",
+    "url": "/naveen-db/index.html"
+  },
+  {
+    "revision": "1400f9c0f48da8c6dcf3",
+    "url": "/naveen-db/static/css/main.41241d6f.chunk.css"
+  },
+  {
+    "revision": "2699a43325bb0cf352a2",
+    "url": "/naveen-db/static/js/2.d45565ed.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/naveen-db/static/js/2.d45565ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1400f9c0f48da8c6dcf3",
+    "url": "/naveen-db/static/js/main.f7016b97.chunk.js"
+  },
+  {
+    "revision": "668777b1d39967242381",
+    "url": "/naveen-db/static/js/runtime-main.4dc3f7f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/naveen-db/precache-manifest.6829f4f89d19dec17b8aaabe83c0710a.js"
+  "/naveen-db/precache-manifest.407e7efbae8b07fa9ccf1276404f22e3.js"
 );
 
 self.addEventListener('message', (event) => {
