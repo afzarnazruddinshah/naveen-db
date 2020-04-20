ae2a53464bc9d6a7f512700a4a88aea9eeb8cd9b
livingstone pilavendran <livingstone.it@gmail.com>
2020-04-20 14:04:18 +0530

    Updates

---
+++ precache-manifest.f3b8b86ae3a25b5b458223d9e394f089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb4cb74d01b675dede66f4f6410eefa",
+    "url": "/naveen-db/index.html"
+  },
+  {
+    "revision": "af49e21f7ca84bbe2123",
+    "url": "/naveen-db/static/css/main.7a253a09.chunk.css"
+  },
+  {
+    "revision": "ef67ded9d093ad4dae3d",
+    "url": "/naveen-db/static/js/2.936f65a8.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/naveen-db/static/js/2.936f65a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af49e21f7ca84bbe2123",
+    "url": "/naveen-db/static/js/main.11b9d5cc.chunk.js"
+  },
+  {
+    "revision": "668777b1d39967242381",
+    "url": "/naveen-db/static/js/runtime-main.4dc3f7f0.js"
+  }
+]);
